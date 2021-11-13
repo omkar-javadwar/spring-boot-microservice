a77afe380855f470f8a65ec0c25b57b9aa452dc3
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2021-11-13 14:40:43 +0530

    Updated Welcome Service

--- welcome/src/main/java/com/cropdeal/welcome/WelcomeApplication.java
+++ welcome/src/main/java/com/cropdeal/welcome/WelcomeApplication.java
@@ -2,7 +2,13 @@ package com.cropdeal.welcome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@EnableEurekaClient
+@OpenAPIDefinition(info=@Info(title = "Welcome API", version = "1.0", description = "welcome service"))
 @SpringBootApplication
 public class WelcomeApplication {
 
