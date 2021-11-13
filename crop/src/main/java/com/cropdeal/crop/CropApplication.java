da575201c67769ea954f3921302d82cae5882a0d
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2021-11-13 14:45:25 +0530

    Updated Crop Service

--- crop/src/main/java/com/cropdeal/crop/CropApplication.java
+++ crop/src/main/java/com/cropdeal/crop/CropApplication.java
@@ -2,12 +2,17 @@ package com.cropdeal.crop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@EnableEurekaClient
+@OpenAPIDefinition(info=@Info(title = "Crop API", version = "1.0", description = "crop service"))
 @SpringBootApplication
 public class CropApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CropApplication.class, args);
 	}
-
 }
--- crop/src/main/java/com/cropdeal/crop/controller/CropController.java
+++ crop/src/main/java/com/cropdeal/crop/controller/CropController.java
@@ -9,38 +9,40 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cropdeal.crop.model.Crop;
 import com.cropdeal.crop.service.CropService;
 
 @RestController
+@RequestMapping("/crop")
 public class CropController {
 
 	@Autowired
 	private CropService cropService;
 
-	@GetMapping("/crop/all")
+	@GetMapping("/all")
 	public List<Crop> viewCrops() {
 		return cropService.viewCrops();
 	}
 
-	@GetMapping("/crop/{id}")
+	@GetMapping("/{id}")
 	public Crop viewCrop(@PathVariable("id") String cropId) {
 		return cropService.viewCrop(cropId);
 	}
 
-	@PostMapping("/crop")
+	@PostMapping("")
 	public Crop addCrop(@RequestBody Crop crop) {
 		return cropService.addCrop(crop);
 	}
 	
-	@PutMapping("/crop/{id}")
+	@PutMapping("/{id}")
 	public Crop updateCrop(@RequestBody Crop crop, @PathVariable("id") String cropId) {
 		return cropService.updateCrop(crop, cropId);
 	}
 	
-	@DeleteMapping("/crop/{id}")
+	@DeleteMapping("/{id}")
 	public String deleteCrop(@PathVariable("id") String cropId) {
 		return cropService.deleteCrop(cropId);
 	}
